b01f03aad61a0d03bbc6e75dc50baf0b79c4313f
Joseph Petersen <josephp90@gmail.com>
2020-02-27 23:46:05 +0100

    avoid sudo

--- wsl.sh
+++ wsl.sh
@@ -4,21 +4,21 @@ VER=$(lsb_release -sr) # like `18.04`
 # dotnet
 
 wget -q https://packages.microsoft.com/config/ubuntu/${VER}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 rm -f packages-microsoft-prod.deb
 
-sudo apt update
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1 dotnet-runtime-2.1
+apt update
+apt install -y apt-transport-https
+apt update
+apt install -y dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1 dotnet-runtime-2.1
 
 # pwsh
 
-sudo apt-get install -y powershell-preview
-sudo ln -sf /usr/bin/pwsh-preview /usr/bin/pwsh
+apt-get install -y powershell-preview
+ln -sf /usr/bin/pwsh-preview /usr/bin/pwsh
 
 # python
-sudo apt install python3 python3-venv python3-pip
+apt install python3 python3-venv python3-pip
 
 # gpg
 
