e18a0a30eff94ce8a212acd2303631613b6a0d37
Joseph Petersen <josephp90@gmail.com>
2020-02-29 18:33:53 +0100

    output wsl distro to temp folder

--- wsldistro.ps1
+++ wsldistro.ps1
@@ -1,4 +1,4 @@
-$file = ".\ubuntu-1804.appx"
+$file = "$env:TEMP\ubuntu-1804.appx"
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile $file -UseBasicParsing
 Add-AppxPackage $file
 Remove-Item $file
