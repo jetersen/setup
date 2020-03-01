6ac3ea99381f0e6236198443513de2c0519ab5a3
Joseph Petersen <josephp90@gmail.com>
2020-03-01 12:49:21 +0100

    setup ruby and nodejs in wsl

--- wsl.sh
+++ wsl.sh
@@ -21,7 +21,17 @@ apt install -y powershell-preview
 ln -sf /usr/bin/pwsh-preview /usr/bin/pwsh
 
 # python
+
 apt install -y python3 python3-venv python3-pip
+
+# ruby
+
+apt install -y ruby ruby-dev ruby-bundler zlib1g-dev
+
+# nodejs
+
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+apt install -y nodejs
 
 # gpg
 
