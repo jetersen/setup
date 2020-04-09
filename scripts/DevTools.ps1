bfe998953bf045d2ee518aa4e3bff691a4ff977d
Joseph Petersen <josephp90@gmail.com>
2020-04-09 14:42:17 +0200

    add netfx-4.8-devpack

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -1,6 +1,7 @@
 Write-Host "Installing DevTools"
 choco install -y dotnetcore-sdk --version=2.1.805 --side-by-side
 choco install -y dotnetcore-sdk --version=3.1.201 --side-by-side
+choco install -y netfx-4.8-devpack
 choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
 choco install -y python3
 choco install -y adoptopenjdk11
