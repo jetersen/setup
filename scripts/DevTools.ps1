8e65a5b75f1117fb287b2ceea513b4233bb785c7
Joseph Petersen <josephp90@gmail.com>
2020-08-06 00:01:07 +0200

    UTF-8 everything

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -12,3 +12,16 @@ choco install -y rapidee
 choco install -y slack
 choco install -y jetbrainstoolbox
 choco install -y microsoft-windows-terminal
+
+# UTF-8!
+
+$CodePageProperties=@{
+  ACP=65001
+  MACCP=65001
+  OEMCP=65001
+}
+
+foreach ($Item in $CodePageProperties.Keys)
+{
+  New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Nls\CodePage -Name $Item -PropertyType String -Value $CodePageProperties[$Item] -Force
+}
