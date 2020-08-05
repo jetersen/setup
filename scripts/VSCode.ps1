0bfc095c48616f32fa454784840b887f220d96a8
Joseph Petersen <josephp90@gmail.com>
2020-08-05 23:50:57 +0200

    install vscode extensions

--- scripts/VSCode.ps1
+++ scripts/VSCode.ps1
@@ -1,7 +1,27 @@
 Write-Host "Installing VSCode and co"
 choco install -y vscode
-choco install -y vscode-yaml
-choco install -y vscode-powershell
-choco install -y vscode-docker
-choco install -y vscode-editorconfig
-choco install -y vscode-python
+RefreshEnv
+$extensions = @(
+  'dbaeumer.vscode-eslint',
+  'eamodio.gitlens',
+  'EditorConfig.EditorConfig',
+  'file-icons.file-icons',
+  'hashicorp.terraform',
+  'ionutvmi.reg', # windows registry files
+  'mitchdenny.ecdc', # encode decode
+  'ms-azuretools.vscode-docker',
+  'ms-python.python',
+  'ms-vscode-remote.remote-ssh',
+  'ms-vscode-remote.remote-ssh-edit',
+  'ms-vscode-remote.remote-wsl',
+  'ms-vscode.powershell',
+  'ms-vsliveshare.vsliveshare',
+  'oderwat.indent-rainbow',
+  'redhat.vscode-xml',
+  'redhat.vscode-yaml',
+  'rust-lang.rust',
+  'yzhang.markdown-all-in-one'
+)
+$extensions = $extensions | Foreach-Object { "--install-extension $_" }
+
+Invoke-Expression "code $extensions"
