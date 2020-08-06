0dbfc3832319034f3074014e063c189141f41418
Joseph Petersen <josephp90@gmail.com>
2020-08-06 11:43:07 +0200

    comma not necessary

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -28,24 +28,24 @@ RefreshEnv
 gsudo config CacheMode Auto
 
 $extensions = @(
-  'dbaeumer.vscode-eslint',
-  'eamodio.gitlens',
-  'EditorConfig.EditorConfig',
-  'file-icons.file-icons',
-  'hashicorp.terraform',
-  'ionutvmi.reg', # windows registry files
-  'mitchdenny.ecdc', # encode decode
-  'ms-azuretools.vscode-docker',
-  'ms-python.python',
-  'ms-vscode-remote.remote-ssh',
-  'ms-vscode-remote.remote-ssh-edit',
-  'ms-vscode-remote.remote-wsl',
-  'ms-vscode.powershell',
-  'ms-vsliveshare.vsliveshare',
-  'oderwat.indent-rainbow',
-  'redhat.vscode-xml',
-  'redhat.vscode-yaml',
-  'rust-lang.rust',
+  'dbaeumer.vscode-eslint'
+  'eamodio.gitlens'
+  'EditorConfig.EditorConfig'
+  'file-icons.file-icons'
+  'hashicorp.terraform'
+  'ionutvmi.reg' # windows registry files
+  'mitchdenny.ecdc' # encode decode
+  'ms-azuretools.vscode-docker'
+  'ms-python.python'
+  'ms-vscode-remote.remote-ssh'
+  'ms-vscode-remote.remote-ssh-edit'
+  'ms-vscode-remote.remote-wsl'
+  'ms-vscode.powershell'
+  'ms-vsliveshare.vsliveshare'
+  'oderwat.indent-rainbow'
+  'redhat.vscode-xml'
+  'redhat.vscode-yaml'
+  'rust-lang.rust'
   'yzhang.markdown-all-in-one'
 )
 $extensions = $extensions | Foreach-Object { "--install-extension $_" }
