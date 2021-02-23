6820d593b6214c8a46421ba22ab5f3044fd15d87
Joseph Petersen <josephp90@gmail.com>
2021-02-23 11:16:47 -0800

    indiviual choco install

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -1,7 +1,29 @@
 choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
-choco install bitwarden-cli maven gpg4win gsudo rapidee microsoft-teams.install jetbrainstoolbox microsoft-windows-terminal
-choco install nuget.commandline gh 7zip vscode pwsh git heidisql element-desktop keybase
-choco install dotnet-sdk docker-desktop googlechrome firefox sops spacesniffer spotify winscp
+choco install bitwarden-cli
+choco install maven
+choco install gpg4win
+choco install gsudo
+choco install rapidee
+choco install microsoft-teams.install
+choco install jetbrainstoolbox
+choco install microsoft-windows-terminal
+choco install nuget.commandline
+choco install gh
+choco install 7zip
+choco install vscode
+choco install pwsh
+choco install git
+choco install heidisql
+choco install element-desktop
+choco install keybase
+choco install dotnet-sdk
+choco install docker-desktop
+choco install googlechrome
+choco install firefox
+choco install sops
+choco install spacesniffer
+choco install spotify
+choco install winscp
 choco install AdoptOpenJDK11 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk11"
 choco install AdoptOpenJDK8 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk8"
 
