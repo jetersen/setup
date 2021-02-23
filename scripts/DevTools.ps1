d0524b1b475964902ce5a44c5f2bf58071cbea02
Joseph Petersen <josephp90@gmail.com>
2021-02-23 21:07:55 +0100

    hmm choco install seems buggy in boxstarter

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -1,30 +1,30 @@
-choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
-choco install bitwarden-cli
-choco install maven
-choco install gpg4win
-choco install gsudo
-choco install rapidee
-choco install microsoft-teams.install
-choco install jetbrainstoolbox
-choco install microsoft-windows-terminal
-choco install nuget.commandline
-choco install gh
-choco install 7zip
-choco install vscode
-choco install pwsh
-choco install git
-choco install heidisql
-choco install keybase
-choco install dotnet-sdk
-choco install docker-desktop
-choco install googlechrome
-choco install firefox
-choco install sops
-choco install spacesniffer
-choco install spotify
-choco install winscp
-choco install AdoptOpenJDK11 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk11"
-choco install AdoptOpenJDK8 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk8"
+# choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
+# choco install bitwarden-cli
+# choco install maven
+# choco install gpg4win
+# choco install gsudo
+# choco install rapidee
+# choco install microsoft-teams.install
+# choco install jetbrainstoolbox
+# choco install microsoft-windows-terminal
+# choco install nuget.commandline
+# choco install gh
+# choco install 7zip
+# choco install vscode
+# choco install pwsh
+# choco install git
+# choco install heidisql
+# choco install keybase
+# choco install dotnet-sdk
+# choco install docker-desktop
+# choco install googlechrome
+# choco install firefox
+# choco install sops
+# choco install spacesniffer
+# choco install spotify
+# choco install winscp
+# choco install AdoptOpenJDK11 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk11"
+# choco install AdoptOpenJDK8 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment /INSTALLDIR=C:\Program Files\AdoptOpenJDK\jdk8"
 
 RefreshEnv
 
