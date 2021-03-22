d0580d1226fb816061771b679d62a72ad4662a89
Joseph Petersen <josephp90@gmail.com>
2021-03-22 17:04:52 +0100

    fix url to personal projects

--- scripts/Projects.ps1
+++ scripts/Projects.ps1
@@ -1,8 +1,8 @@
 $folder = "C:\git\code"
 New-Item $folder -ItemType Directory
 Set-location $folder
-git clone https://github.com/casz/dotfiles.git
-git clone https://github.com/casz/setup.git
+git clone https://github.com/jetersen/dotfiles.git
+git clone https://github.com/jetersen/setup.git
 
 $folder = "C:\git\work"
 New-Item $folder -ItemType Directory
