8327cedbab2aa08c8655fadec317a7776ee662ac
Joseph Petersen <josephp90@gmail.com>
2021-03-22 17:40:44 +0100

    clone projects without set-location

--- scripts/Projects.ps1
+++ scripts/Projects.ps1
@@ -1,8 +1,7 @@
 $folder = "C:\git\code"
 New-Item $folder -ItemType Directory
-Set-location $folder
-git clone https://github.com/jetersen/dotfiles.git
-git clone https://github.com/jetersen/setup.git
+git clone https://github.com/jetersen/dotfiles.git "$folder\dotfiles"
+git clone https://github.com/jetersen/setup.git "$folder\setup"
 
 $folder = "C:\git\work"
 New-Item $folder -ItemType Directory
