0ec571d897f2c9a68d4f7ee38b7849928664d008
Joseph Petersen <josephp90@gmail.com>
2021-08-11 14:45:26 +0200

    use choco install for wsl ubuntu 2004

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -5,17 +5,9 @@ if ($winVer -ge 2004) {
   wsl --set-default-version 2
 }
 if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
-  $item = "wslubuntu2004"
-  $file = "$env:TEMP\$item.appx"
-  Write-Host "Downloading $item"
-  curl.exe -sL https://aka.ms/$item -o $file
-  Add-AppxPackage $file
-  Remove-Item $file
+  choco install wsl-ubuntu-2004 --params "/InstallRoot:true"
 
   RefreshEnv
-
-  Ubuntu2004 install --root
-  Ubuntu2004 config --default-user root
   Ubuntu2004 run "curl -sL '$helperUri/WSL.sh' | bash"
   Ubuntu2004 config --default-user joseph
 }
