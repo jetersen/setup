a6773160ae1adfcf4c271f5d1b98f3e9cee0ec2e
Joseph Petersen <josephp90@gmail.com>
2021-12-31 02:54:00 +0100

    specifially install ubuntu LTS 2004

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,6 +1,6 @@
 wsl --set-default-version 2
 if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
-  winget install --exact --silent "Canonical.Ubuntu"
+  winget install --exact --silent "Canonical.Ubuntu.2004"
   ubuntu2004.exe install --root
   Ubuntu2004 run "curl -sL '$helperUri/WSL.sh' | bash"
   Ubuntu2004 config --default-user joseph
