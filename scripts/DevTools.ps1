a585ef903d5114b74d3bbf61174faf287839ce67
Joseph Petersen <josephp90@gmail.com>
2022-09-01 11:36:39 +0200

    accept agreements

--- scripts/DevTools.ps1
+++ scripts/DevTools.ps1
@@ -25,12 +25,12 @@ $PackageIdentifiers = @(
   "Yubico.Authenticator"
 )
 
-$wingetList = winget list
+$wingetList = winget list --accept-source-agreements
 foreach ($package in $PackageIdentifiers) {
   if ($wingetList | Select-String -Pattern $package.Replace('+', '\+') -Quiet) {
     "$package already installed"
   } else {
     "Installing $package"
-    winget install --exact $package
+    winget install --exact $package --accept-package-agreements
   }
 }
