bc1b1821e15785f1d54d85f370f81489b864cd6a
Joseph Petersen <me@jetersen.dev>
2023-06-08 17:48:59 +0200

    install dotfiles in user script

--- scripts/user.sh
+++ scripts/user.sh
@@ -1,3 +1,5 @@
 mkdir -p "/home/joseph/git/work" "/home/joseph/git/code"
 
 git clone https://github.com/jetersen/dotfiles.git "/home/joseph/git/code/dotfiles"
+
+/home/joseph/git/code/dotfiles/install.sh
