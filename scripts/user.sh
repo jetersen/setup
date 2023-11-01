36947b8605e23d0fa96504aec98b70b3ba7fb8f3
Joseph Petersen <josephp90@gmail.com>
2023-11-01 18:59:43 +0100

    eval brew shellenv

--- scripts/user.sh
+++ scripts/user.sh
@@ -5,5 +5,6 @@ git clone https://github.com/jetersen/dotfiles.git "/home/joseph/git/code/dotfil
 /home/joseph/git/code/dotfiles/install.sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install awscli kubernetes-cli helm kustomize sops yq jandedobbeleer/oh-my-posh/oh-my-posh fluxcd/tap/flux
