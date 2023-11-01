5d7f88e4ec59459a2e3e4f732872d1fd4b7ed802
Joseph Petersen <josephp90@gmail.com>
2023-11-01 18:57:41 +0100

    Install brew as user

--- scripts/user.sh
+++ scripts/user.sh
@@ -3,3 +3,7 @@ mkdir -p "/home/joseph/git/work" "/home/joseph/git/code"
 git clone https://github.com/jetersen/dotfiles.git "/home/joseph/git/code/dotfiles"
 
 /home/joseph/git/code/dotfiles/install.sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install awscli kubernetes-cli helm kustomize sops yq jandedobbeleer/oh-my-posh/oh-my-posh fluxcd/tap/flux
