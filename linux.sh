6f05391871409d2760b56764511a0823b6e1a4a5
Joseph Petersen <me@jetersen.dev>
2024-11-09 12:56:12 +0100

    install git if not present during Fedora setup

--- linux.sh
+++ linux.sh
@@ -2,6 +2,11 @@
 source /etc/os-release
 if [[ $ID == "fedora" ]]; then
     echo "This is Fedora 🤠"
+    if ! command -v git &> /dev/null; then
+      echo "git could not be found, installing..."
+      sudo dnf install -y git
+    fi
+
     mkdir -p $HOME/git/code
     git clone https://github.com/jetersen/setup.git $HOME/git/code/setup
     $HOME/git/code/setup/scripts/fedora.sh
